843fbe7b16bbad0dc3cad2af9f526f9e17c08a28
Lukács Dávid <17357366+justdawee@users.noreply.github.com>
2024-10-22 15:35:55 +0200

    Initial commit

---
+++ ZH_PROG2/LukacsDavid1ZH_HHC62Y/LukacsDavid1ZH_HHC62Y/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace LukacsDavid1ZH_HHC62Y
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startBtn = new System.Windows.Forms.Button();
+            this.speedTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(3, 3);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(120, 60);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // speedTrackBar
+            // 
+            this.speedTrackBar.Location = new System.Drawing.Point(19, 393);
+            this.speedTrackBar.Maximum = 100;
+            this.speedTrackBar.Minimum = 1;
+            this.speedTrackBar.Name = "speedTrackBar";
+            this.speedTrackBar.Size = new System.Drawing.Size(769, 45);
+            this.speedTrackBar.TabIndex = 1;
+            this.speedTrackBar.Value = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.speedTrackBar);
+            this.Controls.Add(this.startBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Brush Button (ZH feladat.)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TrackBar speedTrackBar;
+
+        private System.Windows.Forms.Button startBtn;
+
+        #endregion
+    }
+}
---
+++ ZH_PROG2/LukacsDavid1ZH_HHC62Y/LukacsDavid1ZH_HHC62Y/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LukacsDavid1ZH_HHC62Y
+{
+    public partial class Form1 : Form
+    {
+        private Point _direction;
+        private Rectangle _bounds;
+        private bool _isMoving = false;
+        private CancellationTokenSource _cts;
+        private readonly Random _random = new Random();
+        
+        private readonly List<(Rectangle rectangle, Pen penColor)> _storedRects = new List<(Rectangle, Pen)>(5192);
+        
+        // Készítette: Lukács Dávid Roland (HHC62Y)
+        // 2024.10.07.
+        // Made with JetBrains Rider 2024
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            startBtn.Left = (ClientSize.Width - startBtn.Width) / 2; //képernyő közepére a gombot
+            startBtn.Top = (ClientSize.Height - startBtn.Height) / 2;
+            _bounds = new Rectangle(0, 0, ClientSize.Width, ClientSize.Height-speedTrackBar.Height-20);
+            _direction = new Point(_random.Next(2) * 2 - 1, _random.Next(2) * 2 - 1);
+            
+            //ezek azért kellenek hogy ne villogjon az egész
+            DoubleBuffered = true; 
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+        }
+
+        private async void startBtn_Click(object sender, EventArgs e)
+        {
+            if (!_isMoving) //ha nem mozog, akkor elindítjuk
+            {
+                startBtn.Text = "Stop";
+                _cts = new CancellationTokenSource();
+                _direction = new Point(_random.Next(2) == 0 ? 1 : -1, _random.Next(2) == 0 ? 1 : -1);
+                _isMoving = true;
+                await StartMoving(_cts.Token);
+            }
+            else //ha mozog, akkor leállítjuk
+            {
+                _cts.Cancel();
+                startBtn.Text = "Start";
+                _isMoving = false;
+            }
+        }
+        
+        private async Task StartMoving(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                MoveButton(); // mozgatjuk a gombot
+                try
+                {
+                    await Task.Delay(1000 / speedTrackBar.Value, token); //a sebesség a trackbar értékétől függ
+                }
+                catch (TaskCanceledException)
+                {
+                    // ignored
+                }
+            }
+        }
+        
+        private void MoveButton()
+        {
+            Point newLocation = startBtn.Location;
+            newLocation.X += _direction.X * 2;
+            newLocation.Y += _direction.Y * 2;
+            
+            if (newLocation.X < _bounds.Left || newLocation.X + startBtn.Width > _bounds.Right)
+            {
+                _direction.X = -_direction.X; //ha elérte a széleket, akkor megfordítjuk az irányát
+            }
+            if (newLocation.Y < _bounds.Top || newLocation.Y + startBtn.Height > _bounds.Bottom)
+            {
+                _direction.Y = -_direction.Y;
+            }
+            
+            var penColor = new Pen(Color.FromArgb(_random.Next(256), _random.Next(256), _random.Next(256)));
+            var rectangle = new Rectangle(startBtn.Location.X, startBtn.Location.Y, startBtn.Width, startBtn.Height);
+            
+            _storedRects.Add((rectangle, penColor)); //a gomb új helyét és színét hozzáadjuk a listához
+            startBtn.Location = newLocation;
+            
+            Invalidate();
+        }
+        
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e); //rajzoljuk ki a gombokat
+            foreach (var rectangle in _storedRects)
+            {
+                e.Graphics.FillRectangle(rectangle.penColor.Brush, rectangle.rectangle); //kitöltjük a gombokat
+            }
+            startBtn.Refresh(); //frissítjük a gombot hogy ne tűnjön el
+        }
+    }
+}
